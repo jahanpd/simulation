daff1335a77d510784b802b7e9d350391134d997
jahanpd <jahan.pd@gmail.com>
2016-10-30 16:27:16 +1100

    rename var

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 from random import uniform, randint
 
-__rate__ = 1000000
+Rate = 1000000
 
-__x__ = bin(__rate__)
+Gene = bin(Rate)
 
-__gene__ = __x__[2:].zfill(100) + ('0'*100)
+Genome = Gene[2:].zfill(100) + ('0'*100)
 
-while int(__gene__[0:100], 2) == __rate__:
-    __prob__ = 1./int(__gene__[0:100], 2)
-    __edit__ = ""
-    for n in __gene__:
-        if __prob__ >= uniform(0, 1):
-            __edit__ += str(randint(0, 1))
+while int(Genome[0:100], 2) == Rate:
+    Prob = 1./int(Genome[0:100], 2)
+    _ = ""
+    for n in Genome:
+        if Prob >= uniform(0, 1):
+            _ += str(randint(0, 1))
         else:
-            __edit__ += n
-    __gene__ = __edit__
-    print __gene__
+            _ += n
+    Genome = _
+    print Genome
