b9fd299007bc640d12aa5725da18a44e631157a6
jahanpd <jahan.pd@gmail.com>
2016-10-30 17:42:58 +1100

    yeah

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
 from random import uniform, randint
 
-Rate = 1000000
+Population = []
 
-Gene = bin(Rate)
+for n in range(100):
+    Rate = randint(1000, 100000)
+    Gene = bin(Rate)
+    Genome = Gene[2:].zfill(100) + ('0'*100)
+    Population.append(Genome)
 
-Genome = Gene[2:].zfill(100) + ('0'*100)
+while len(Population) != 0:
+    _Pop = []
+    Dead = []
+    for Genome in Population:
+        Rate = int(Genome[0:100], 2)
+        Prob = 1./Rate
+        _ = ""
+        for n in Genome:
+            if Prob >= uniform(0, 1):
+                _ += str(randint(0, 1))
+            else:
+                _ += n
+        Genome = _
+        Newrate = int(Genome[0:100], 2)
+        if Rate == Newrate:
+            _Pop.append(Genome)
 
-while int(Genome[0:100], 2) == Rate:
-    Prob = 1./int(Genome[0:100], 2)
-    _ = ""
-    for n in Genome:
-        if Prob >= uniform(0, 1):
-            _ += str(randint(0, 1))
-        else:
-            _ += n
-    Genome = _
-    print Genome
+    Population = _Pop
+    print len(Population)
