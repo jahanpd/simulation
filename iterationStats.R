c88da58b8a1d40362f6a06e1c6d41bbe28a9d39d
jahanpd <jahan.pd@gmail.com>
2017-09-12 08:27:41 +1000

    simrun

--- iterationStats.R
+++ iterationStats.R
@@ -20,11 +20,11 @@ iter.data <- function(path, filename.pattern, iterations) {
     data <- matrix.iters[1:number.files,n]
     data <- data[is.na(data) == FALSE]
     if (is.numeric(mean(data))) {
-      iters.avg[n] <- mean(data)
+      iters.avg[n] <- median(data)
       iters.std[n] <- sd(data)
     }
   }
-  return(list(avg = iters.avg, std = iters.std, x = c(1:iterations)))
+  return(list(avg = iters.avg, std = iters.std, x = c(0:(iterations-1))))
 }
 
 path.1 = "~/Documents/Hackz/Simulation/1/data"
@@ -33,13 +33,13 @@ path.0.1 = "~/Documents/Hackz/Simulation/0.1/data"
 path.0.01 = "~/Documents/Hackz/Simulation/0.01/data"
 pattern = "statsRateAvg"
 
-iters.1 = data.frame(iter.data(path.1, pattern, 1000))
-iters.0.4 = data.frame(iter.data(path.0.4, pattern, 1000))
-iters.0.1 = data.frame(iter.data(path.0.1, pattern, 1000))
+iters.1 = data.frame(iter.data(path.1, pattern, 1001))
+iters.0.4 = data.frame(iter.data(path.0.4, pattern, 1001))
+iters.0.1 = data.frame(iter.data(path.0.1, pattern, 1001))
 iters.0.01 = data.frame(iter.data(path.0.01, pattern, 1001))
 
-plotData <- data.frame(cbind(iters.1$avg, iters.0.4$avg, iters.0.1$avg, iters.0.01$avg, iters.1$x))
-colnames(plotData) <- c("1", "0.4", "0.1", "0.01", "x")
+plotData <- data.frame(cbind(iters.1$avg, iters.0.4$avg, iters.0.1$avg, iters.0.01$avg, iters.0.01$x))
+colnames(plotData) <- c("1","0.4", "0.1", "0.01", "x")
 plotData <- melt(plotData, id="x")
 
 p <- ggplot(plotData, aes(x, value, colour=variable)) + geom_line()
--- stats.R
+++ stats.R
@@ -10,20 +10,20 @@ density.data <- function(path, filename.pattern, iterations) {
                        header = FALSE,
                        sep=",",
                        colClasses = "numeric")
-    mean <- mean(temp[is.na(temp) == FALSE])
+    mean <- median(temp[is.na(temp) == FALSE])
     #datas[match(n, file.names)] <- mean
     datas[n] <- mean
   }
   return(datas)
 }
 
-density1 <- density.data("~/Documents/Hackz/Simulation/1/data", "statsEnd", 860)
-density0.4 <- density.data("~/Documents/Hackz/Simulation/0.4/data", "statsEnd", 860)
-density0.1 <- density.data("~/Documents/Hackz/Simulation/0.1/data", "statsEnd", 860)
-density0.01 <- density.data("~/Documents/Hackz/Simulation/0.01/data", "statsEnd", 860)
+density1 <- density.data("~/Documents/Hackz/Simulation/1/data", "statsEnd", 1001)
+density0.4 <- density.data("~/Documents/Hackz/Simulation/0.4/data", "statsEnd", 1001)
+density0.1 <- density.data("~/Documents/Hackz/Simulation/0.1/data", "statsEnd", 1001)
+density0.01 <- density.data("~/Documents/Hackz/Simulation/0.01/data", "statsEnd", 1001)
 
-densities <- data.frame(density0.1, density0.4, density1, density0.01)
-colnames(densities) <- c("0.1", "0.4", "1", "0.01")
+densities <- data.frame(density1, density0.4, density0.1, density0.01)
+colnames(densities) <- c("1", "0.4", "0.1", "0.01")
 densities <- melt(densities)
 
 p <- ggplot(densities, aes(value, fill = variable)) + 
@@ -36,7 +36,7 @@ p$labels$colour <- "P(kill)"
 p + theme_minimal()
 
 p <- ggplot(densities, aes(value, fill = variable)) + 
-  geom_histogram(binwidth = 0.1, alpha=0.5) + 
+  geom_histogram(binwidth = 0.4, alpha=0.5) + 
   xlim(min(densities$value), 100)
 p$labels$x <- "Mutation Index (Lifespan)"
 p$labels$y <- "Density"
@@ -44,6 +44,13 @@ p$labels$fill <- "P(kill)"
 p$labels$colour <- "P(kill)"
 p + theme_minimal()
 
+medians = c(median(density0.01),median(density0.1),median(density0.4),median(density1))
+prs = c(0.01,0.1,0.4,1)
+plotting = data.frame(medians, prs)
+linear.model <- lm(prs ~ medians, plotting)
+
+p <- ggplot(plotting, aes(medians,prs)) + geom_point()
+p +  geom_smooth(method="lm", formula = y ~ exp(-x))
 
 
 high.normal = shapiro.test(high)
